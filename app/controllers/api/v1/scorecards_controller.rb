d5df294a812e1ed97ecda1a23562730479624eac
dkersten <dkersten1994@gmail.com>
2020-07-30 14:48:25 -0400

    added delete/destroy method to scorecard

--- app/controllers/api/v1/scorecards_controller.rb
+++ app/controllers/api/v1/scorecards_controller.rb
@@ -13,6 +13,27 @@ module Api
                 render json: scorecard.to_json
             end
 
+            def new
+                Scorecard.new
+            end
+
+            def create
+                scorecard = Scorecard.create(scorecard_params)
+                render json: scorecard.to_json
+            end
+
+            def destroy
+                scorecard = Scorecard.find(params[:id])
+                scorecard.destroy
+                render json: {message: "Scorecard deleted"}
+            end
+
+            private
+            # need to figure out a better solution for this
+                def scorecard_params
+                    params.require(:scorecard).permit!
+                end
+
         end
     end
 end
