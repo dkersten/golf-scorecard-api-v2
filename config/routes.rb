ce8e909ae2a4f5972cef356deec0073604d65f78
dkersten <dkersten1994@gmail.com>
2020-08-07 13:43:35 -0400

    refactoring routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,29 @@
 Rails.application.routes.draw do
+
+  # resources :private
+
   namespace :api do
     
     namespace :v1 do
 
       # get '/users/:id/holes_played', to: 'users#holesPlayed'
 
-      resources :users
+      # resources :users
+      get '/users/me', to: 'users#me'
     end
 
     namespace :v1 do
       resources :scorecards
     end
 
+    namespace :v1 do
+      resources :private
+    end
+
+  end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
   end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
